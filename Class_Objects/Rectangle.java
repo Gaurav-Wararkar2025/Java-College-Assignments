dede6932fc272a0812a89ef3141921af122c6eeb
Gaurav Wararkar <gauravwararkar143@gmail.com>
2026-03-17 01:21:51 +0530

    Question 6

---
+++ Class_Objects/Rectangle.java
@@ -0,0 +1,32 @@
+/*
+#Classes and Objects in Java:
+
+6. Write a program to print the area of two rectangles having sides (4,5) and (5,8) respectively 
+by creating a class named 'Rectangle' with a method named 'Area' which returns the area and length 
+and breadth passed as parameters to its constructor.
+
+ */
+
+class Rectangle {
+
+    int length;
+    int breadth;
+
+    Rectangle(int l, int b) {
+        length = l;
+        breadth = b;
+    }
+
+    int Area() {
+        return length * breadth;
+    }
+
+    public static void main(String args[]) {
+
+        Rectangle r1 = new Rectangle(4, 5);
+        Rectangle r2 = new Rectangle(5, 8);
+
+        System.out.println("Area of Rectangle 1: " + r1.Area());
+        System.out.println("Area of Rectangle 2: " + r2.Area());
+    }
+}
