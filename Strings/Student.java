e5ac6fcdec435f228d9218c337fa1424f6de8955
Gaurav Wararkar <gauravwararkar143@gmail.com>
2026-03-27 13:31:59 +0530

    Question 9

---
+++ Strings/Student.java
@@ -0,0 +1,34 @@
+/*
+# Strings in Java:
+
+9. Define a class Student with attributes id and name.
+
+Override toString() method.
+
+Create object of class.
+
+Display object details using toString().
+ 
+*/
+
+class Student {
+
+    int id;
+    String name;
+
+    Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String toString() {
+        return "ID: " + id + ", Name: " + name;
+    }
+
+    public static void main(String[] args) {
+
+        Student s = new Student(101, "Rahul");
+
+        System.out.println(s); // calls toString()
+    }
+}
